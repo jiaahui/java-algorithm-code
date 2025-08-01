6f5d30e4d4bb0b33f2a41b28bbb4e1032e9eb928
lijiahui <lijiahuius@gmail.com>
2025-08-01 11:04:48 +0800

    add addTwoNumbers

--- zuochengyun/ListAlgorithm.java
+++ zuochengyun/ListAlgorithm.java
@@ -3,6 +3,7 @@ package zuochengyun;
 // 链表
 // 反转单链表、反转双链表
 // 合并两个有序列表
+// 两个链表相加
 
 // Java API: LinkedList
 
@@ -91,5 +92,35 @@ public class ListAlgorithm {
         pre.next = cur1 != null ? cur1 : cur2;
         
         return head;
+    }
+
+    // 两个链表相加
+    // https://leetcode.cn/problems/add-two-numbers
+    public static ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+        ListNode ans = null, cur = null;
+        int carry = 0;  // 进位
+
+        for (int sum, val; 
+             head1 != null || head2 != null; 
+             head1 = head1 == null ? null : head1.next, head2 = head2 == null ? null : head2.next) {
+            
+            sum = (head1 == null ? 0 : head1.val) + (head2 == null ? 0 : head2.val) + carry;
+            val = sum % 10;
+            carry = sum / 10;
+
+            if (ans = null) {
+                ans = new ListNode(val);
+                cur = ans;
+            } else {
+                cur.next = new ListNode(val);
+                cur = cur.next;
+            }
+        }
+
+        if (carry == 1) {
+            cur.next = new ListNode(1);
+        }
+
+        return ans;
     }
 }
