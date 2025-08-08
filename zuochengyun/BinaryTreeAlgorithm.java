da353972ea522f2aa70138163ef9a55bab94e9b8
lijiahui <lijiahuius@gmail.com>
2025-08-08 18:20:53 +0800

    add BinaryTreeTraversalIteration

--- zuochengyun/BinaryTreeAlgorithm.java
+++ zuochengyun/BinaryTreeAlgorithm.java
@@ -4,6 +4,10 @@ package zuochengyun;
 // 递归遍历：前序遍历、中序遍历、后序遍历
 // 非递归遍历：前序遍历、中序遍历、后序遍历
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BinaryTreeAlgorithm {
     public static class TreeNode {
         public int val;
@@ -37,5 +41,89 @@ public class BinaryTreeAlgorithm {
         preOrderTraversalRecursion(head.left);
         preOrderTraversalRecursion(head.right);
         System.out.print(head.val + " ");
+    }
+
+    // 前序遍历迭代实现
+    // https://leetcode.cn/problems/binary-tree-preorder-traversal
+    public static List<Integer> preOrderTraversalIteration(TreeNode head) {
+        List<Integer> ans = new ArrayList<>();
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(head);
+
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                ans.add(head.val);
+                if (head.right != null) stack.push(head.right);  // 先右后左
+                if (head.left != null) stack.push(head.left);
+            }
+        }
+        return ans;
+    }
+
+    // 中序遍历迭代实现
+    // https://leetcode.cn/problems/binary-tree-inorder-traversal
+    public static List<Integer> inOrderTraversalIteration(TreeNode head) {
+        List<Integer> ans = new ArrayList<>();
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    ans.add(head.val);
+                    head = head.right;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // 后序遍历迭代实现 方法一：使用两个栈
+    // https://leetcode.cn/problems/binary-tree-postorder-traversal
+    public static List<Integer> postOrderTraversalIteration(TreeNode head) {
+        List<Integer> ans = new ArrayList<>();
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            Stack<TreeNode> collect = new Stack<>();
+
+            stack.push(head);
+
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                collect.push(head);
+
+                if (head.left != null) stack.push(head.left);  // 先左后右
+                if (head.right != null) stack.push(head.right);
+            }
+
+            while (!collect.isEmpty()) {ans.add(collect.pop().val);}
+        }
+        return ans;
+    }
+
+    // 后序遍历迭代实现 方法一：使用一个栈
+    // https://leetcode.cn/problems/binary-tree-postorder-traversal
+    public static List<Integer> postOrderTraversalIteration(TreeNode head) {
+        List<Integer> ans = new ArrayList<>();
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(head);
+
+            while (!stack.isEmpty()) {
+                TreeNode cur = stack.peek();
+
+                if (cur.left != null && head != cur.left && head != cur.right) stack.push(cur.left);
+                else if (cur.right != null && head != cur.right) stack.push(cur.right);
+                else {
+                    ans.add(cur.val);
+                    head = stack.pop();
+                }
+            }
+        }
+        return ans;
     }
 }
